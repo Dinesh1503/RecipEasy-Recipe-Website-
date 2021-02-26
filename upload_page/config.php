05539e3197cacb928a451b106a766995850f91df
wayoasis <66959001+wayoasis@users.noreply.github.com>
2021-02-26 19:54:44 +0000

    upload_page

---
+++ upload_page/config.php
@@ -0,0 +1,9 @@
+
+<?php
+    $servername = "dbhost.cs.man.ac.uk";
+    $username   = "e95562sp";
+    $password   = "STORED_recipes+";
+    $database   = "2020_comp10120_y14";
+
+	$conn = mysqli_connect($servername, $username, $password, $database);
+?>
