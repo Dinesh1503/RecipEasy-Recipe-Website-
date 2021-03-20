9c8f72fbaa424626edefe09934fc2a594f147ab7
Yunyi Zhang <yunyi.zhang-2@student.manchester.ac.uk>
2021-03-20 09:32:36 +0000

    updated upload

--- upload_page/processing.php
+++ upload_page/processing.php
@@ -1,7 +1,7 @@
 
 <?php 
 
-include_once("config.php");
+include("../DB/config.php");
 
 $file = $_FILES["pictureInput"];
 $title = $_POST["titleInput"];
@@ -10,29 +10,27 @@ $time = $_POST["timeInput"];
 $serving = $_POST["servingInput"];
 $category = $_POST["categoryInput"];
 $ingredients = $_POST["ingredientsInput"];
-$uploadedBy = 0; //user id
+$uploadedBy = $_SESSION['user_id']; //user id
 
+// move uploaded img to uploads/
 $dir = "uploads/";
-
 $filePath = $dir . uniqid() . basename($file["name"]);
 
 $filePath = str_replace(" ", "_", $filePath);
 $type = pathinfo($filePath, PATHINFO_EXTENSION);
 
-
 move_uploaded_file($file["tmp_name"], $filePath);
 
-// add ingredients, description, time, category columns
-$sql = "INSERT INTO Recipe(ingredients, description, time, category, image_url, title, number_of_servings, user_id, original_site_url, calories, total_weight, total_nutrients, total_daily, diet_labels, health_labels)
-VALUES('$ingredients', '$description', '$time', '$category', '$filePath', '$title', '$serving', '$uploadedBy','', 0, 0, '', '', '', '')";
+$sql = "INSERT INTO Recipe(ingredients, description, time, 	cuisine_type, image_url, title, number_of_servings, user_id)
+VALUES('$ingredients', '$description', '$time', '$category', '$filePath', '$title', '$serving', '$uploadedBy')";
 
 if($conn->query($sql))
 {
-    echo("Uploaded sucessfully");
+    echo("<h3 id='uploadSucess' style='text-align:center;'>Uploaded sucessfully<h3>");
+    header( "refresh:2;url=http://localhost:8888/y14-group-project-master%208/Static/upload.php" );
 } 
 else{
     echo("Error: " . $conn->error);
 }
-
 
 ?>
