4134f4f5d478ab22088e5d0e6bc4c1395c747222
SamP-S <sam.ps1@outlook.com>
2021-03-23 11:00:30 +0000

    Fix css on recipe display page

--- Main/recipe.php
+++ Main/recipe.php
@@ -1,21 +1,18 @@
-<?php	
+<?php
 	session_start();
 	session_regenerate_id();
 	require_once("main.php");
-
-	$recipe_id = $_GET["recipe_id"];
 	
-	$URL = "https://api.spoonacular.com/recipes/" . $recipe_id. "/information" . "?" . API_KEY;
+    $recipe_id = $_GET["recipe_id"];
+    $URL = "https://api.spoonacular.com/recipes/" . $recipe_id. "/information" . "?" . API_KEY;
 	$json_string = makeCURL($URL);
 	$recipe = json_decode($json_string);
+    $content = show_recipe($recipe);  
 
-	$content = show_recipe($recipe);
 	$layout = new Template("index.tpl");
 	$layout->set("title", "TEMPLATE");
 	$layout->set("user", getUserElements());
 	$layout->set("content", $content);
 	
 	echo($layout->output());
-
-
-?>
+?>	
