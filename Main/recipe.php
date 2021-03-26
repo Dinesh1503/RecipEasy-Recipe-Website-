9a1a1bc63f2a35d83c8de7e128971492b13d0bdc
Yunyi Zhang <yunyi.zhang-2@student.manchester.ac.uk>
2021-03-26 16:46:58 +0000

    move api calls to main showRecipe()

--- Main/recipe.php
+++ Main/recipe.php
@@ -1,13 +1,13 @@
+
 <?php
 	session_start();
 	session_regenerate_id();
 	require_once("main.php");
-	
-    $recipe_id = $_GET["recipe_id"];
-    $URL = "https://api.spoonacular.com/recipes/" . $recipe_id. "/information" . "?" . API_KEY;
-	$json_string = makeCURL($URL);
-	$recipe = json_decode($json_string);
-    $content = show_recipe($recipe);  
+
+    $recipe_id = strval($_GET["recipe_id"]);
+    
+	// only if not exist, call api
+	$content = show_recipe($recipe_id);
 
 	$layout = new Template("index.tpl");
 	$layout->set("title", "TEMPLATE");
@@ -16,3 +16,4 @@
 	
 	echo($layout->output());
 ?>	
+
