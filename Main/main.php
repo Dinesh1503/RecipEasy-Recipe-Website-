0c2a7fac4f97e9bfacaabc890aaa440145ca9d33
SamP-S <sam.ps1@outlook.com>
2021-04-15 18:49:48 +0100

    Finalised Database Structure

--- Main/main.php
+++ Main/main.php
@@ -415,130 +415,6 @@
 
 	}
 
-	class RecipeForm {
-
-        public function createUploadForm() {
-
-            $pictureInput = $this->createPictureInput();
-            $titleInput = $this->createTitleInput();
-            $ingredientsInput = $this->createIngredientsInput();
-            $servingInput = $this->createServingInput();
-            $timeInput = $this->createTimeInput();
-
-            $categoriesInput = $this->createCategoriesInput();
-
-            $descriptionInput = $this->createDescriptionInput();
-            $uploadButton = $this->createUploadButton();
-
-            return "
-			<link rel=\"stylesheet\" type=\"text/css\" href=\"css/upload.css\">
-			<h1 >Upload Your Own Recipes!</h1>
-			<div class=\"column\">
-			<form action='processing.php' method='POST' enctype='multipart/form-data'>
-
-			$pictureInput
-
-			<div class = 'row'>
-
-				<div class='col'>
-				$categoriesInput
-				</div>
-
-				<div class='col'>
-				$servingInput
-				</div>
-
-				<div class='col'>
-				$timeInput
-				</div>
-
-			</div>
-
-			$titleInput
-			$ingredientsInput
-			$descriptionInput
-			$uploadButton
-
-			</form>";
-
-        }
-
-
-
-        private function createPictureInput() {
-
-            return "<div class='form-group'>
-                <label for='photo'>Your photo</label><br>
-                <input type='file' accept='.jpeg, .jpg, .png, .gif, .bmp, .webp, .raw, .ico, .tiff'
-                class='form-control-file w-50' id='photo' name='pictureInput' required>
-                <br><br>
-
-                </div>";
-                }
-
-        private function createTitleInput() {
-            return "<div class='form-group'>
-            <br>
-            <input class='form-control' type='text' placeholder='Title' name='titleInput' required>
-            </div>";
-        }
-
-        private function createIngredientsInput() {
-            return "<div class='form-group'>
-            <input class='form-control' type='text' placeholder='Ingredients' pattern = '(\w+)(,*\s*\w+)*' name='ingredientsInput' required>
-            </div>";
-        }
-
-        private function createDescriptionInput() {
-            return "<div class='form-group'>
-            <textarea class='textarea-control' style='resize: none;' placeholder='Instruction' name='descriptionInput' rows='10' required></textarea>
-            </div>";
-        }
-
-        private function createServingInput() {
-
-            return "<div class='outline'>
-            <label class='form-label' for='serveNum'>Number of Servings</label>
-            <input type='number' min='1' max='6' id='serveNum' name='servingInput' class='form-control input-sm' required/>
-            </div>";
-                }
-
-        private function createTimeInput() {
-
-            return "<div class='outline'>
-            <label class='form-label' for='cookTime'>Cooking Time (min)</label>
-            <input type='number' min='0' id='cookTime' name='timeInput' class='form-control input-sm' required/>
-            </div>";
-                }
-
-        private function createCategoriesInput() {
-
-            $html = "<div class='form-group'>
-            <label class='form-label' for='select'>Cuisine Type</label>
-            <select class='form-select' aria-label='Default select example' name='categoryInput' id='select' required>";
-
-            for($i=0;$i<=11;$i++) {
-
-                $category = ['British', 'French', 'Italian', 'Central Europe',
-                'Caribbean', 'Eastern Europe', 'Chinese', 'Japanese', 'Indian',
-                'South East Asia', 'American', 'Mexican'];
-
-                $html .= "<option value=$category[$i]>$category[$i]</option>";
-
-            }
-
-            $html .= "</select></div>";
-
-            return $html;
-
-
-        }
-
-        private function createUploadButton() {
-            return "<div class='col text-center'><button type='submit' class='btn btn-primary' name='uploadButton'>Upload</button>
-            </div>";
-        }
-    }
 
 	class Template {
     	/**
@@ -652,7 +528,7 @@
 
 
 			for ($i = 0; $i < $ingredientslength; $i++) {
-				$sql = "INSERT INTO fridge2 (userID, INGREDIENT)
+				$sql = "INSERT INTO fridge2 (user_id, INGREDIENT)
 								VALUES ('$id','$ingredients[$i]')
 								";
 								if ($conn->query($sql))
@@ -665,7 +541,7 @@
 								}
 			}
 
-			$sql = "SELECT INGREDIENT FROM fridge2 where userID = $id";
+			$sql = "SELECT INGREDIENT FROM fridge2 where user_id = $id";
 			$result = $conn->query($sql);
 
 			if ($result->num_rows > 0) {
@@ -707,7 +583,7 @@
 
   		$userid = $_SESSION["id"];
 
-  		$sql ="SELECT * FROM fridge2 WHERE userID = $userid";
+  		$sql ="SELECT * FROM fridge2 WHERE user_id = $userid";
   		$result = $conn->query($sql);
 
   		$output = "";
@@ -752,7 +628,7 @@
 
 			$userid = $_SESSION["id"];
 
-			$sql = "SELECT * FROM fridge2 WHERE userID = $userid";
+			$sql = "SELECT * FROM fridge2 WHERE user_id = $userid";
 			$result = $conn->query($sql);
 
 
@@ -772,14 +648,14 @@
 				$new = $change[1];
 
 				// if($new = "remove"){
-				// 	$sql = "DELETE FROM fridge2 WHERE (userID = $userid AND INGREDIENT = '$orignal')";
+				// 	$sql = "DELETE FROM fridge2 WHERE (user_id = $userid AND INGREDIENT = '$orignal')";
 				// 	if ($conn->query($sql))
 				// 	{
 				// 		echo ("Record deleted successfully");
 				// 	}
 				// }
 				// else{
-				$sql = "UPDATE fridge2 SET INGREDIENT = '$new' WHERE (userID = $userid AND INGREDIENT = '$orignal')";
+				$sql = "UPDATE fridge2 SET INGREDIENT = '$new' WHERE (user_id = $userid AND INGREDIENT = '$orignal')";
 					if (!$conn->query($sql)) {
 						echo("Error: " . $conn->error);
 					}
@@ -810,14 +686,14 @@
 		$sql = "
 		WITH cte AS (
 			SELECT
-				userID,
+				user_id,
 				INGREDIENT,
 				ROW_NUMBER() OVER (
 					PARTITION BY
-						userID,
+						user_id,
 						INGREDIENT
 					ORDER BY
-						userID,
+						user_id,
 						INGREDIENT
 				) row_num
 			 FROM
@@ -848,7 +724,7 @@
 
 		$userid = $_SESSION["id"];
 
-		$sql ="INSERT INTO fridge2 (userID, INGREDIENT) VALUES ($userid, '$item')";
+		$sql ="INSERT INTO fridge2 (user_id, INGREDIENT) VALUES ($userid, '$item')";
 
 		$result = $conn->query($sql);
 		if (!$result) {
@@ -872,7 +748,7 @@
 
 		$userid = $_SESSION["id"];
 
-		$sql ="DELETE FROM fridge2 WHERE userID = $userid AND INGREDIENT = '$item'";
+		$sql ="DELETE FROM fridge2 WHERE user_id = $userid AND INGREDIENT = '$item'";
 
 		$result = $conn->query($sql);
 		if (!$result) {
@@ -895,7 +771,7 @@
 		}
 		$userid = $_SESSION["id"];
 
-		$sql ="SELECT * FROM fridge2 WHERE userID = $userid";
+		$sql ="SELECT * FROM fridge2 WHERE user_id = $userid";
 		$result = $conn->query($sql);
 		$items = array();
 		if ($result->num_rows > 0) {
