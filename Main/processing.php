2fae7e40f5cb182cdfb3b8acad0cfe74e51a4176
SamP-S <sam.ps1@outlook.com>
2021-04-16 00:05:58 +0100

    Fix monkey nuts bug

--- Main/processing.php
+++ Main/processing.php
@@ -19,23 +19,6 @@ $serving = $_POST["servingInput"];
 $time = $_POST["timeInput"];
 $instructions = $_POST["instructionsInput"];
 
-# check if any diets set
-$diet_array = array();
-if (isset($_POST["diets"])) {
-	$diet_array = $_POST["diets"];
-}
-# convert to string for SQL
-$diets = "";
-if (count($diet_array) == 0) {
-	$diets = "NULL";	
-} else {
-	$diets = $diet_array[0];
-	for ($i = 1; $i < count($diet_array); $i++) {
-		$diets = $diets . "," . $diet_array[$i];
-	}
-	$diets = "'$diets'" ;
-}
-
 # check if any intolerances set
 $intolerances_array = array();
 if (isset($_POST["intolerances"])) {
@@ -53,6 +36,22 @@ if (count($intolerances_array) == 0) {
 	$intolerances = "'$intolerances'";
 }
 
+# check if any diets set
+$diet_array = array();
+if (isset($_POST["diets"])) {
+	$diet_array = $_POST["diets"];
+}
+# convert to string for SQL
+$diets = "";
+if (count($diet_array) == 0) {
+	$diets = "NULL";	
+} else {
+	$diets = $diet_array[0];
+	for ($i = 1; $i < count($diet_array); $i++) {
+		$diets = $diets . "," . $diet_array[$i];
+	}
+	$diets = "'$diets'" ;
+}
 
 $dir = "uploads/";
 
@@ -68,12 +67,12 @@ $user_id = intval($_SESSION['id']);
 # make sql 
 $sql = "INSERT INTO Recipe(title, cuisine_type, meal_type, image_url, number_of_servings, ready_in_minutes, instructions, intls, diets, user_id)
 VALUES('$title', '$cuisine', '$meal', '$filePath', '$serving', '$time', '$instructions', $intolerances, $diets, '$user_id')";
-echo($sql);
+echo($sql . "<br>");
 if(!$conn->query($sql)) {
-    echo("Error: " . $conn->error);
+    echo("Error at insert: " . $conn->error . "<br>");
 }
 
-$recipe_check_query = "SELECT * FROM Recipe WHERE (`title` LIKE '%".$title."%') LIMIT 1";
+$recipe_check_query = "SELECT * FROM Recipe ORDER BY recipe_id DESC LIMIT 1";
 $result = mysqli_query($conn, $recipe_check_query);
 $recipe = mysqli_fetch_assoc($result);
 
@@ -84,11 +83,12 @@ $ingredients = explode(", ", $ingredientsInput);
 
 for ($i = 0; $i < count($ingredients); $i++) {
  	$sql = "INSERT INTO Ingredients(recipe_id, ingr_name, ingr_amount, ingr_unit)
-			VALUES ('$recipe_id', '$ingredients[$i]', '0', 'unit', )";
-   	$conn->query($sql);
+			VALUES ('$recipe_id', '$ingredients[$i]', '0', 'unit')";
+   	if (!$conn->query($sql)) {
+		echo($conn->error . "<br>");
+	}
 }
 
 header("Location: index.php");
-
 
 ?>
