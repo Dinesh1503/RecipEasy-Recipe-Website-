de64002ab8b1ed429e991175e15caa0a0d1f4794
a65242ak <angel.kodjaivanov@student.manchester.ac.uk>
2021-04-16 13:37:34 +0100

    Fix show recipe when searching by ingredients

--- Main/searchIngredients.php
+++ Main/searchIngredients.php
@@ -34,18 +34,37 @@
 		$json = json_decode($json_string);
 		console_log("$json_string");
 
+		if(isset($_POST['number'])){
+			$number_of_results = $_POST['number'];
+		}
+		else{
+			$number_of_results = 10;
+		}
+		if(isset($_POST['offset'])){
+			$offset = $_POST['offset'];
+		}
+		else{
+			$offset = 0;
+		}
+
+		$shown_recipes = array();
 		$results = "";
 
 
 		if (count($json) != 0) {
-			for ($i = 0; $i < count($json); $i++) {
+			if(($offset > count($json) - $number_of_results) && ($json - $number_of_results >= 0)){
+				$offset = $json - $number_of_results;
+			}
+			for ($i = $offset; $i < $number_of_results + $offset && $i < count($json); $i++) {
 				$recipe = $json[$i];
-
-				$result = new Template("elements/searchResult.tpl");
-				$result->set("link", "recipe.php/?recipe_id=$recipe->id");
-				$result->set("title", "$recipe->title");
-				$result->set("img", "$recipe->image");
-				$results = $results . $result->output();
+				if(!in_array($recipe->title, $shown_recipes)){
+					array_push($shown_recipes, $recipe->title);
+					$result = new Template("elements/searchResult.tpl");
+					$result->set("link", "redirect_to_recipe.php/?recipe_id=$recipe->id");
+					$result->set("title", "$recipe->title");
+					$result->set("img", "$recipe->image");
+					$results = $results . $result->output();
+				}
 			}
 			$grid->set("results", $results);
 		} else {
